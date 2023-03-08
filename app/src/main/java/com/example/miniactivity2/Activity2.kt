c05601f1a94c790e68bf0a2e60fce0f49c2a20e4
Pere <peremunozfiguerol33@gmail.com>
2023-03-08 18:36:21 +0100

    implemented the logic between the two screens

--- app/src/main/java/com/example/miniactivity2/Activity2.kt
+++ app/src/main/java/com/example/miniactivity2/Activity2.kt
@@ -1,5 +1,6 @@
 package com.example.miniactivity2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.miniactivity2.databinding.Activity2Binding
@@ -9,5 +10,17 @@ class Activity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = Activity2Binding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val byeMessage = intent.getStringExtra("byeMessage")
+        val repetitions = intent.getIntExtra("repetitions", 0)
+
+        binding.byeText.text = byeMessage?.repeat(repetitions)
+
+        binding.backButton.setOnClickListener {
+            val intent = Intent()
+            intent.putExtra("byeMessage", binding.byeText.text.toString())
+            setResult(RESULT_OK, intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/miniactivity2/MainActivity.kt
+++ app/src/main/java/com/example/miniactivity2/MainActivity.kt
@@ -1,13 +1,58 @@
 package com.example.miniactivity2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import com.example.miniactivity2.databinding.ActivityMainBinding
+
+lateinit var startForResult : ActivityResultLauncher<Intent>
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.buttonToByeScreen.setOnClickListener {
+            val intent = Intent(this, Activity2::class.java)
+
+            if (binding.editTextNumber.text.isBlank() || binding.editTextNumber.text.isBlank()) {
+
+                if (binding.editTextNumber.text.isEmpty()) {
+                    binding.editTextNumber.error = "Please enter a number"
+                }
+
+                if (binding.editTextByeMessage.text.isEmpty()) {
+                    binding.editTextByeMessage.error = "Please enter a message"
+                }
+
+            } else if (binding.editTextNumber.text.toString().toInt() <= 0) {
+                binding.editTextNumber.error = "Please enter a number greater than 0"
+
+            } else {
+
+                val byeMessage = binding.editTextByeMessage.text.toString()
+                val repetitions = binding.editTextNumber.text.toString().toInt()
+                intent.putExtra("byeMessage", byeMessage)
+                intent.putExtra("repetitions", repetitions)
+                startForResult.launch(intent)
+
+            }
+        }
+
+        startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            if (result.resultCode == RESULT_OK) {
+                val intent = result.data
+                val byeMessage = intent?.getStringExtra("byeMessage")
+                binding.saluteMessage.text = byeMessage
+
+                binding.editTextNumber.text.clear()
+                binding.editTextByeMessage.text.clear()
+                binding.editTextByeMessage.requestFocus()
+            }
+        }
     }
 }
